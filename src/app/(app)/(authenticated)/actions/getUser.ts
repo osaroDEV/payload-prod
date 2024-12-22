c649113afe20522a80650c579cb3c582461924cf
osarodev <osernon@gmail.com>
2024-12-22 20:20:39 +0100

    refactor getUsers II

--- src/app/(app)/(authenticated)/actions/getUser.ts
+++ src/app/(app)/(authenticated)/actions/getUser.ts
@@ -6,24 +6,39 @@ import configPromise from '@payload-config'
 import type { Payload } from 'payload'
 import { Customer } from '@/payload-types'
 
+// export async function getUser(): Promise<Customer | null> {
+//   const headers = await getHeaders()
+//   const payload: Payload = await getPayload({ config: await configPromise })
+//   const { user } = await payload.auth({ headers })
+
+//   return user || null
+// }
+
 export async function getUser(): Promise<Customer | null> {
   const headers = await getHeaders()
   const payload: Payload = await getPayload({ config: await configPromise })
-  
-  const authHeader = headers.get('authorization')
-  if (!authHeader) {
-    return null
+  const { user } = await payload.auth({ headers })
+
+  // Use the UsersSelect interface to select only the firstName and lastName fields
+  interface UsersSelect<T> {
+    firstName: string;
+    lastName: string;
+  }
+  if (!user) {
+    return null;
   }
 
-  const token = authHeader.split(' ')[1]
-  const user = await payload.find({
-    collection: 'customers',
-    where: {
-      token: {
-        equals: token,
-      },
-    },
-  })
+  const { firstName, lastName } = user as UsersSelect<{ firstName: string; lastName: string }>
 
-  return user.docs[0] || null
+  // Create a new Customer object with the selected fields
+  const customer: Customer = { 
+    id: user.id,
+    updatedAt: user.updatedAt,
+    createdAt: user.createdAt,
+    email: user.email,
+    firstName, 
+    lastName 
+  }
+
+  return customer
 }
